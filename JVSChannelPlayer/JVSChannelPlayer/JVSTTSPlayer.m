7e8aadece775bd5b0777319d219b219c627e5ace
Jon Schang <jon_schang@yahoo.com>
2016-06-11 06:10:55 -0700

    tracking items by utterance in a dict.  we'll see how that goes.

--- JVSChannelPlayer/JVSChannelPlayer/JVSTTSPlayer.m
+++ JVSChannelPlayer/JVSChannelPlayer/JVSTTSPlayer.m
@@ -18,6 +18,7 @@
 
 @interface JVSTTSPlayer() 
 @property (nonatomic,retain) dispatch_queue_t dispatchQueue;
+@property (nonatomic,retain) NSMutableDictionary* itemsByUtter;
 @end
 
 @implementation JVSTTSPlayer
@@ -29,6 +30,7 @@
     if(!self) return self;
     self.synth = [[AVSpeechSynthesizer alloc] init];
     self.synth.delegate = self;
+    self.itemsByUtter = [[NSMutableDictionary alloc] init];
     //self.voices = [AVSpeechSynthesisVoice speechVoices];
     dispatchQueue = dispatch_queue_create("JVSTTSPlayer", DISPATCH_QUEUE_CONCURRENT);
     self.currentItem = nil;
@@ -58,8 +60,10 @@
         if(self.currentItem) {
             self.currentItem = item;
             utter = [AVSpeechUtterance speechUtteranceWithString:item.text];
+            [_itemsByUtter setObject:item forKey:utter.description];
             //utter.voice = self.voice;
             [synth speakUtterance:utter];
+            NSLog(@"utterance.description=%@",utter.description);
         }
     });
 }
@@ -87,37 +91,46 @@
 
 -(void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didPauseSpeechUtterance:(AVSpeechUtterance *)utterance {
     dispatch_async(dispatch_get_main_queue(),^(){
-        if(self.delegate && self.currentItem && [self.delegate respondsToSelector:@selector(player:didPauseItem:)]) {
-            [self.delegate player:self didPauseItem:self.currentItem];
+        id<JVSTTSPlayerItem> item = _itemsByUtter[utterance.description];
+        if(self.delegate && item && [self.delegate respondsToSelector:@selector(player:didPauseItem:)]) {
+            [self.delegate player:self didPauseItem:item];
         }
     });
 }
 -(void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didStartSpeechUtterance:(AVSpeechUtterance *)utterance {
     dispatch_async(dispatch_get_main_queue(),^(){
-        if(self.delegate && self.currentItem && [self.delegate respondsToSelector:@selector(player:didBeginItem:)]) {
-            [self.delegate player:self didBeginItem:self.currentItem];
-        }
-    });
-}
--(void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didCancelSpeechUtterance:(AVSpeechUtterance *)utterance {
-    dispatch_async(dispatch_get_main_queue(),^(){
-        if(self.delegate && self.currentItem && [self.delegate respondsToSelector:@selector(player:didStopItem:)]) {
-            [self.delegate player:self didStopItem:self.currentItem];
-        }
-    });
-}
--(void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didFinishSpeechUtterance:(AVSpeechUtterance *)utterance {
-    dispatch_async(dispatch_get_main_queue(),^(){
-        if(self.delegate && self.currentItem && [self.delegate respondsToSelector:@selector(player:didFinishItem:)]) {
-            [self.delegate player:self didFinishItem:self.currentItem];
+        id<JVSTTSPlayerItem> item = _itemsByUtter[utterance.description];
+        if(self.delegate && item && [self.delegate respondsToSelector:@selector(player:didBeginItem:)]) {
+            [self.delegate player:self didBeginItem:item];
         }
     });
 }
 -(void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didContinueSpeechUtterance:(AVSpeechUtterance *)utterance {
     dispatch_async(dispatch_get_main_queue(),^(){
-        if(self.delegate && self.currentItem && [self.delegate respondsToSelector:@selector(player:didResumeItem:)]) {
-            [self.delegate player:self didResumeItem:self.currentItem];
+        id<JVSTTSPlayerItem> item = _itemsByUtter[utterance.description];
+        if(self.delegate && item && [self.delegate respondsToSelector:@selector(player:didResumeItem:)]) {
+            [self.delegate player:self didResumeItem:item];
         }
+    });
+}
+-(void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didCancelSpeechUtterance:(AVSpeechUtterance *)utterance {
+    dispatch_async(dispatch_get_main_queue(),^(){
+        id<JVSTTSPlayerItem> item = _itemsByUtter[utterance.description];
+        if(self.delegate && item && [self.delegate respondsToSelector:@selector(player:didStopItem:)]) {
+            [self.delegate player:self didStopItem:item];
+        }
+        NSLog(@"removing utterance item:%@",utterance.description);
+        [_itemsByUtter removeObjectForKey:utterance.description];
+    });
+}
+-(void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didFinishSpeechUtterance:(AVSpeechUtterance *)utterance {
+    dispatch_async(dispatch_get_main_queue(),^(){
+        id<JVSTTSPlayerItem> item = _itemsByUtter[utterance.description];
+        if(self.delegate && item && [self.delegate respondsToSelector:@selector(player:didFinishItem:)]) {
+            [self.delegate player:self didFinishItem:item];
+        }
+        NSLog(@"removing utterance item:%@",utterance.description);
+        [_itemsByUtter removeObjectForKey:utterance.description];
     });
 }
 -(void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer willSpeakRangeOfSpeechString:(NSRange)characterRange utterance:(AVSpeechUtterance *)utterance {
